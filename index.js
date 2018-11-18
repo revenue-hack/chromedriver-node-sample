ba8c8a7e9875b6dae5b432e3dc227c3300a7e509
Kohei Ozeki <K.odeveloper10@gmail.com>
2018-11-19 08:58:22 +0900

    [modified] change logic en route

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 'use strict';
 
-const url = 'https://news.yahoo.co.jp/';
-var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
+const url = 'https://www.skincare-univ.com';
 
-var options = new chrome.Options();
-var logging_prefs = new webdriver.logging.Preferences();
-logging_prefs.setLevel(webdriver.logging.Type.PERFORMANCE, webdriver.logging.Level.ALL);
-options.setLoggingPrefs(logging_prefs);
-options.addArguments('headless');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
-var driver = new webdriver.Builder().withCapabilities(options.toCapabilities()).build();
+const prefs = new webdriver.logging.Preferences();
+prefs.setLevel(webdriver.logging.Type.PERFORMANCE, webdriver.logging.Level.ALL);
+const caps = webdriver.Capabilities.chrome();
+caps.set('chromeOptions', {
+  args: [
+    '--headless',
+    '--no-sandobox',
+  ]
+});
+caps.setLoggingPrefs(prefs)
+const driver = new webdriver.Builder().withCapabilities(caps).build();
 
 driver.get(url);
 driver.manage().logs().get('performance').then(function(text) {
-    console.log(text);
+  console.log('テキスト', text);
 });
 driver.quit();
+
